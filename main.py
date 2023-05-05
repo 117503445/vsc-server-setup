096d6b021b5a47b14090ceb86d0d992e57ad71a5
HaoTian Qi <qihaotian@hyperchain.cn>
2023-05-05 18:19:25 +0800

    fix: error handle for ssh
    Signed-off-by: HaoTian Qi <qihaotian@hyperchain.cn>

--- main.py
+++ main.py
@@ -85,45 +85,47 @@ if not file_dest.exists():
     if r.status_code != 200:
         logger.error(f'download failed, status_code = {r.status_code}')
         alert(
-            f'download tag_name[{tag_name}] failed, sha = {sha}, bin_url = {bin_url}')
+            f'download vsc server[{tag_name}] failed, sha = {sha}, bin_url = {bin_url}')
         exit(1)
     with open(file_dest, 'wb') as f:
         f.write(r.content)
-    alert(f'download vsc server tag_name[{tag_name}] success')
+    alert(f'download vsc server[{tag_name}] success')
 
 logger.info('processing targets')
 
 for target in cfg.get('targets', []):
-    logger.info(f'target = {target}')
-    client: paramiko.client.SSHClient = paramiko.client.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    username = target['user']
-    client.connect(target['host'], username=username)
-
-    if username == 'root':
-        home_dir = '/root'
-    else:
-        home_dir = f'/home/{username}'
-
-    ftp_client = client.open_sftp()
-    is_exist = False
     try:
-        ftp_client.stat(f'{home_dir}/.vscode-server/bin/{sha}/node')
-        is_exist = True
-    except IOError as ex:
-        logger.debug(ex)
-        pass
+        logger.info(f'target = {target}')
+        client: paramiko.client.SSHClient = paramiko.client.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        username = target['user']
+        client.connect(target['host'], username=username)
 
-    logger.info(f'is_exist = {is_exist}')
+        if username == 'root':
+            home_dir = '/root'
+        else:
+            home_dir = f'/home/{username}'
 
-    if not is_exist:
-        ftp_client.put(file_dest, '/tmp/vscode-server-linux-x64.tar.gz')
-        command = f'rm -rf ~/.vscode-server/bin/{sha} && mkdir -p ~/.vscode-server/bin/{sha} && echo extra to ~/.vscode-server/bin/{sha} && tar --no-same-owner -zx --strip-components=1 -C ~/.vscode-server/bin/{sha} -f "/tmp/vscode-server-linux-x64.tar.gz"'
-        logger.info(f'exec command: {command}')
-        _, _stdout, _ = client.exec_command(command)
-        logger.info(f'output: {_stdout.read().decode()}')
+        ftp_client = client.open_sftp()
+        is_exist = False
+        try:
+            ftp_client.stat(f'{home_dir}/.vscode-server/bin/{sha}/node')
+            is_exist = True
+        except IOError as ex:
+            logger.debug(ex)
+            pass
 
-    ftp_client.close()
-    # _, _stdout, _ = client.exec_command(command)
-    # print(_stdout.read().decode())
-    client.close()
+        logger.info(f'is_exist = {is_exist}')
+
+        if not is_exist:
+            ftp_client.put(file_dest, '/tmp/vscode-server-linux-x64.tar.gz')
+            command = f'rm -rf ~/.vscode-server/bin/{sha} && mkdir -p ~/.vscode-server/bin/{sha} && echo extra to ~/.vscode-server/bin/{sha} && tar --no-same-owner -zx --strip-components=1 -C ~/.vscode-server/bin/{sha} -f "/tmp/vscode-server-linux-x64.tar.gz"'
+            logger.info(f'exec command: {command}')
+            _, _stdout, _ = client.exec_command(command)
+            logger.info(f'output: {_stdout.read().decode()}')
+    except Exception as ex:
+        logging.error(f'failed when processing {target}, exception: {ex}')
+    finally:
+        ftp_client.close()
+
+        client.close()
